62248f941dc6043a5592daffbc11174141af64b8
ethanhuang03 <ethanhuang301@gmail.com>
2022-11-15 16:38:17 +1300

    updated arms library

--- src/main.cpp
+++ src/main.cpp
@@ -55,8 +55,51 @@ void autonomous() {}
 
 
 void opcontrol() {
+	bool master_expansion = false;
+	bool partner_expansion = false;
+	bool aimbot = false;
+	bool overide = false;
+
 	while(true) {
 		arms::chassis::tank(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y), master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+
+		// Manual Overide //
+		if( master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && 
+			master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) && 
+			master.get_digital(pros::E_CONTROLLER_DIGITAL_R1) && 
+			master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) ) {
+			overide = true;
+		}
+		else {
+			overide = false;
+		}
+
+		// Expansion Logic //
+		if ( !overide ) {
+			if( master.get_digital(pros::E_CONTROLLER_DIGITAL_X) ) {
+				master_expansion = !master_expansion;
+			}
+			if( partner.get_digital(pros::E_CONTROLLER_DIGITAL_X) ) {
+				partner_expansion = !partner_expansion;
+			}
+			if( master_expansion && partner_expansion ) {
+				piston(expansion, false, true);
+			}
+		}
+		else {
+			piston(expansion, false, true);
+		}
+
+
+
 		pros::delay(10);
 	}
 }
+
+
+/*
+CONTROLLER DISPLAY
+Aimbot: True/False
+Expansion State: True/False, True/False
+Overide Features: True/False
+*/
